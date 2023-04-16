648e1c3abf446d217cf5fad3734f43ec75c92478
NadiaPia <206i417@gmail.com>
2023-04-15 23:32:06 -0700

    Add style to the login and registration pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
 
@@ -10,38 +10,39 @@ function Login() {
     const [passwordLog, setPasswordLog] = useState("");
 
     const [loginStatus, setLoginStatus] = useState("")
-    
+
     const login = () => {
         axios.post("http://localhost:3003/auth/login", {
-            username: usernameLog, 
+            username: usernameLog,
             password: passwordLog
         }).then((response) => {
-            console.log("response.data.length", response.data);                      
+            console.log("response.data.length", response.data);
             //{id: 1, username: 'nadia', password: 'password', createdAt: '2023-04-14T23:16:07.000Z', updatedAt: '2023-04-14T23:16:07.000Z'}
             //{message: 'User does not exist'}     
-            response.data.message? setLoginStatus(response.data.message) : setLoginStatus(response.data.username)
+            response.data.message ? setLoginStatus(response.data.message) : setLoginStatus(response.data.username)
             //navigate("/")
 
-        }) .catch(error => {
+        }).catch(error => {
             console.log(error)
         })
     }
 
-  return (
-    <div>
-    <div className="login">
-        <h1>Login</h1>
-        
-        <input type="text" placeholder="Username" onChange={(e) => {setUsernameLog(e.target.value)}}/>
-        
-        <input type="text" placeholder="Password..." onChange={(e) => {setPasswordLog(e.target.value)}}/>
+    return (
+        <div>
+            <h1>{loginStatus}</h1>
+            <div className="loginContainer">
+                <h1>Login</h1>
 
-        <button onClick={login}>Login</button>
-      
-    </div>
-    <h1>{loginStatus}</h1>
-    </div>
-  )
+                <input type="text" placeholder="Username" onChange={(e) => { setUsernameLog(e.target.value) }} />
+
+                <input type="text" placeholder="Password..." onChange={(e) => { setPasswordLog(e.target.value) }} />
+
+                <button onClick={login}>Login</button>
+
+            </div>
+            
+        </div>
+    )
 }
 
 export default Login
