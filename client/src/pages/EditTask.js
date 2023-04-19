4d4d315fc7966750852293438e5aa5a14dbb235c
NadiaPia <206i417@gmail.com>
2023-04-19 00:19:06 -0700

    Change icons in the EditTask page, Add onClick event to the X icon

--- client/src/pages/EditTask.js
+++ client/src/pages/EditTask.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-import {faSquareCheck}  from '@fortawesome/free-solid-svg-icons';
+import {faSquareCheck}  from '@fortawesome/free-regular-svg-icons';
+import {faXmarkCircle}  from '@fortawesome/free-regular-svg-icons';
+
+
+
 //<FontAwesomeIcon icon="fa-regular fa-square-check" />
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,12 +13,12 @@ function EditTask(props) {
     
   return (
     <div>
-        <input type="checkbox"/>
+        
         
             <input type="text" value={props.correctedTask} onChange={(event) => {props.setCorrectedTask(event.target.value)}}/> 
             
             <FontAwesomeIcon icon={faSquareCheck} />
-            <FontAwesomeIcon icon={faTrashCan} onClick={() => props.setTaskMode("delete")}/>
+            <FontAwesomeIcon icon={faXmarkCircle} onClick={()=> props.setTaskMode("show")}/>
       
     </div>
   )
