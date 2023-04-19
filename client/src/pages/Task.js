1a881a63cd23a6384f7f580229e0969123b19998
NadiaPia <206i417@gmail.com>
2023-04-18 23:53:47 -0700

    Add comment to the Task.js

--- client/src/pages/Task.js
+++ client/src/pages/Task.js
@@ -7,7 +7,8 @@ import EditTask from "./EditTask";
 function Task(props) {
 
     const [taskMode, setTaskMode] = useState("show") // show, edit, delete, checkbox
-    const [correctedTask, setCorrectedTask] = useState(props.task.taskText)
+    const [correctedTask, setCorrectedTask] = useState(props.task.taskText) //this state must be here to be unic for every task.
+    //if it live in TasksList it will be common for all tasks
 
 
     return (
