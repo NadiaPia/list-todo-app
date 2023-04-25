1c26c22e251a94be45d2c1d483b39d84ed08d5ad
NadiaPia <206i417@gmail.com>
2023-04-25 15:10:50 -0700

    Clean up and eslint the server side

--- server/routes/Tasks.js
+++ server/routes/Tasks.js
@@ -4,49 +4,40 @@ const { TasksLists } = require("../models");
 
 //    "/tasks" = "/"
 
-router.get("/", async(req, res) => {
-    //console.log('HEADERS --->>> ', req.headers)
-    console.log("req.headers.userid --->>> ", req.headers.userid)
-    const listOfTasks = await TasksLists.findAll({where: {UserId: req.headers.userid}})
+router.get("/", async (req, res) => {
+    const listOfTasks = await TasksLists.findAll({ where: { UserId: req.headers.userid } })
     res.json(listOfTasks);
-    //console.log("listOfTasks", listOfTasks)
-   
 });
 
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
     const newTask = req.body; //{textTask: "eat"}
     //console.log("req.headers.userid", req.headers.userid) //2
-    newTask.UserId = req.headers.userid; //{textTask: "eat", UserId: 2}
-    //console.log("newTask///////////////////////////////////////", newTask);
+    newTask.UserId = req.headers.userid; //{textTask: "eat", UserId: 2}   
     const task = await TasksLists.create(newTask);
-    console.log("task", task);
     res.json(task) //{id: 11, taskText: 'water a plant', UserId: '2', updatedAt: 2023-04-11T07:32:25.314Z,  createdAt: 2023-04-11T07:32:25.314Z }
+});
 
-})
-
-router.delete("/:taskId", async(req, res) => {
+router.delete("/:taskId", async (req, res) => {
     const taskId = req.params.taskId
     await TasksLists.destroy({
         where: {
             id: taskId,
         }
-    })
+    });
     res.json("DELETED SUCCESFULLY")
-})
+});
 
-router.put("/:taskId", async(req, res) => {
+router.put("/:taskId", async (req, res) => {
     const taskId = req.params.taskId;
-    console.log(req.body)
-    await TasksLists.update({taskText: req.body.correctedTask}, {where: {id: taskId}});
+    await TasksLists.update({ taskText: req.body.correctedTask }, { where: { id: taskId } });
     res.json("success");
 });
 
-router.put("/checkbox/:taskId", async(req, res) => {
-    console.log("req.body.checkboxreq.body.checkboxreq.body.checkbox", req.body.checkbox)
+router.put("/checkbox/:taskId", async (req, res) => {
+    //console.log("req.body.checkbox", req.body.checkbox)
     const taskId = req.params.taskId;
-    await TasksLists.update({checkbox: req.body.checkbox}, {where: {id: taskId}});
+    await TasksLists.update({ checkbox: req.body.checkbox }, { where: { id: taskId } });
     res.json(req.body.checkbox);
-    
-})
+});
 
-module.exports = router
+module.exports = router;
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -4,30 +4,29 @@ const { Users } = require("../models");
 
 //     "/auth" = "/"
 
-router.post("/", async(req,res) => {
-    const {username, password} = req.body;
-    const user = await Users.create({username: username, password:password})
-    console.log("user.id", user.id);    
-    res.json({username: user.username, id: user.id})
-})
+router.post("/", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await Users.create({ username: username, password: password });
+    //console.log("user.id", user.id);
+    res.json({ username: user.username, id: user.id });
+});
 
-router.post("/login", async(req,res) => {
-    const {username, password} = req.body;
-    const user = await Users.findOne({where: {username: username, password:password}}); //my password is not hashed => I can find it in the db together with the username
-    if(!user) {
-        res.json({message: "User does not exist"})
-    } 
-    res.json({username: user.username, id: user.id}) //we don't need  sent the rest of the object like {password: 'password', createdAt: '2023-04-14T23:16:07.000Z', updatedAt: '2023-04-14T23:16:07.000Z'}
-})
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await Users.findOne({ where: { username: username, password: password } }); //my password is not hashed => I can find it in the db together with the username
+    if (!user) {
+        res.json({ message: "User does not exist" });
+    }
+    res.json({ username: user.username, id: user.id }); //we don't need  sent the rest of the object like {password: 'password', createdAt: '2023-04-14T23:16:07.000Z', updatedAt: '2023-04-14T23:16:07.000Z'}
+});
 
 router.get("/auth", async (req, res) => {
-    console.log('REQ ACCESSS --->>> ', req.headers.accesss)
-    const user = await Users.findOne({where: {id: req.headers.accesss}});
-    if(!user) {
-        res.json({message: "User is not loged in"})
+    //console.log('REQ ACCESSS --->>> ', req.headers.accesss);
+    const user = await Users.findOne({ where: { id: req.headers.accesss } });
+    if (!user) {
+        res.json({ message: "User is not loged in" });
     }
-    res.json({username: user.username, id: user.id})
-})
+    res.json({ username: user.username, id: user.id });
+});
 
-
-module.exports = router
+module.exports = router;
